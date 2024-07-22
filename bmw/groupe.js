c045e36bde2052f37d89e3d7b6ea42e88e15bad6
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-23 00:08:26 +0300

    Update groupe.js

--- bmw/groupe.js
+++ bmw/groupe.js
@@ -31,7 +31,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        🌟 *Bmw-Md* 🌟
+         *Bugatti* 
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Autor : *${nomAuteurMessage}* 👋 
